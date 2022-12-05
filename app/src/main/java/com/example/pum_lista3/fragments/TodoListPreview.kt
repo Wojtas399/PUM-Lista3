794ff6e15b2c1fbf8e36978d0c2f27b52c78dfa3
Wojtas399 <wojtekp26@gmail.com>
2022-12-05 22:53:44 +0100

    Navigation configuration.

--- app/src/main/java/com/example/pum_lista3/fragments/TodoListPreview.kt
+++ app/src/main/java/com/example/pum_lista3/fragments/TodoListPreview.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.pum_lista3.R
 
-class ToDoListCollection : Fragment() {
+class TodoListPreview : Fragment() {
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
     // Inflate the layout for this fragment
     return inflater.inflate(
-      R.layout.fragment_to_do_list_collection,
+      R.layout.fragment_todo_list_preview,
       container,
       false
     )
