f84fd64b54d3b14e8783ebd56edb0cec45cb338f
Wojtas399 <wojtekp26@gmail.com>
2022-12-08 09:40:41 +0100

    Set listeners for value changes in list creator.

--- app/src/main/java/com/example/pum_lista3/fragments/TodoListCreator.kt
+++ app/src/main/java/com/example/pum_lista3/fragments/TodoListCreator.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.example.pum_lista3.R
 import com.example.pum_lista3.databinding.FragmentTodoListCreatorBinding
+import java.time.LocalDate
 
 class TodoListCreator : Fragment() {
   private lateinit var binding: FragmentTodoListCreatorBinding
+  var listNumber: Int? = null
+  var date: LocalDate? = null
+  var description: String? = null
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,9 @@ class TodoListCreator : Fragment() {
     binding = FragmentTodoListCreatorBinding.inflate(inflater, container, false)
 
     setupDropdownItem()
+    setListNumberValueListener()
+    setDateValueListener()
+    setDescriptionValueListener()
 
     return binding.root
   }
@@ -27,6 +35,24 @@ class TodoListCreator : Fragment() {
     val listNumbers = resources.getStringArray(R.array.list_numbers)
     val arrayAdapter =
       ArrayAdapter(requireContext(), R.layout.dropdown_item, listNumbers)
-    binding.todoListNumberTextView.setAdapter(arrayAdapter)
+    binding.listNumberInput.setAdapter(arrayAdapter)
+  }
+
+  private fun setListNumberValueListener() {
+    binding.listNumberInput.setOnItemClickListener { _, _, itemIndex, _ ->
+      listNumber = itemIndex + 1
+    }
+  }
+
+  private fun setDateValueListener() {
+    binding.datePicker.setOnDateChangedListener { _, year, month, day ->
+      date = LocalDate.of(year, month, day)
+    }
+  }
+
+  private fun setDescriptionValueListener() {
+    binding.listDescriptionInput.addTextChangedListener {
+      description = it.toString()
+    }
   }
 }
