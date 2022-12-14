1c35a0a20ff16e7681686aa2f9a4dcaae931e6dc
Wojtas399 <wojtekp26@gmail.com>
2022-12-14 07:27:19 +0100

    Added method to todo list creator screen responsible for going back to previous screen.

--- app/src/main/java/com/example/pum_lista3/todoListCreator/TodoListCreator.kt
+++ app/src/main/java/com/example/pum_lista3/todoListCreator/TodoListCreator.kt
@@ -9,65 +9,77 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.pum_lista3.R
 import com.example.pum_lista3.databinding.FragmentTodoListCreatorBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.time.LocalDate
 
 @AndroidEntryPoint
 class TodoListCreator : Fragment() {
-  private lateinit var binding: FragmentTodoListCreatorBinding
-  private val viewModel: TodoListCreatorViewModel by viewModels()
+    private lateinit var binding: FragmentTodoListCreatorBinding
+    private val viewModel: TodoListCreatorViewModel by viewModels()
 
-  override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    binding = FragmentTodoListCreatorBinding.inflate(inflater, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentTodoListCreatorBinding.inflate(inflater, container, false)
 
-    setToolbarTitle()
-    setupDropdownItem()
-    setListNumberValueListener()
-    setDateValueListener()
-    setDescriptionValueListener()
-    setButtonOnClickListener()
+        setToolbarTitle()
+        setupDropdownItem()
+        setListNumberValueListener()
+        setDateValueListener()
+        setDescriptionValueListener()
+        setButtonOnClickListener()
 
-    return binding.root
-  }
-
-  private fun setToolbarTitle() {
-    requireActivity().findViewById<Toolbar>(R.id.toolbar).title = "Nowa lista"
-  }
-
-  private fun setupDropdownItem() {
-    val listNumbers = resources.getStringArray(R.array.list_numbers)
-    val arrayAdapter =
-      ArrayAdapter(requireContext(), R.layout.dropdown_item, listNumbers)
-    binding.listNumberInput.setAdapter(arrayAdapter)
-  }
-
-  private fun setListNumberValueListener() {
-    binding.listNumberInput.setOnItemClickListener { _, _, itemIndex, _ ->
-      viewModel.changeListNumber(itemIndex + 1)
+        return binding.root
     }
-  }
 
-  private fun setDateValueListener() {
-    binding.datePicker.setOnDateChangedListener { _, year, month, day ->
-      val date: LocalDate = LocalDate.of(year, month + 1, day)
-      viewModel.changeDeadline(date)
+    private fun setToolbarTitle() {
+        requireActivity().findViewById<Toolbar>(R.id.toolbar).title = "Nowa lista"
     }
-  }
 
-  private fun setDescriptionValueListener() {
-    binding.listDescriptionInput.addTextChangedListener {
-      viewModel.changeDescription(it.toString())
+    private fun setupDropdownItem() {
+        val listNumbers = resources.getStringArray(R.array.list_numbers)
+        val arrayAdapter =
+            ArrayAdapter(requireContext(), R.layout.dropdown_item, listNumbers)
+        binding.listNumberInput.setAdapter(arrayAdapter)
     }
-  }
 
-  private fun setButtonOnClickListener() {
-    binding.submitButton.setOnClickListener {
-      viewModel.submit()
+    private fun setListNumberValueListener() {
+        binding.listNumberInput.setOnItemClickListener { _, _, itemIndex, _ ->
+            viewModel.changeListNumber(itemIndex + 1)
+        }
     }
-  }
+
+    private fun setDateValueListener() {
+        binding.datePicker.setOnDateChangedListener { _, year, month, day ->
+            val date: LocalDate = LocalDate.of(year, month + 1, day)
+            viewModel.changeDeadline(date)
+        }
+    }
+
+    private fun setDescriptionValueListener() {
+        binding.listDescriptionInput.addTextChangedListener {
+            viewModel.changeDescription(it.toString())
+        }
+    }
+
+    private fun setButtonOnClickListener() {
+        binding.submitButton.setOnClickListener {
+            runBlocking {
+                launch {
+                    viewModel.submit()
+                }
+            }
+            goBackToPreviousScreen()
+        }
+    }
+
+    private fun goBackToPreviousScreen() {
+        findNavController().popBackStack()
+    }
 }
--- app/src/main/java/com/example/pum_lista3/todoListCreator/TodoListCreatorViewModel.kt
+++ app/src/main/java/com/example/pum_lista3/todoListCreator/TodoListCreatorViewModel.kt
@@ -3,63 +3,59 @@ package com.example.pum_lista3.todoListCreator
 import androidx.lifecycle.ViewModel
 import com.example.pum_lista3.domain.use_cases.AddListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import java.time.LocalDate
 import javax.inject.Inject
 
 data class TodoListCreatorState(
-  val listNumber: Int? = null,
-  val deadline: LocalDate? = null,
-  val description: String? = null,
+    val listNumber: Int? = null,
+    val deadline: LocalDate? = null,
+    val description: String? = null,
 )
 
 @HiltViewModel
 class TodoListCreatorViewModel @Inject constructor(
-  private val addTodoListUseCase: AddListUseCase,
+    private val addTodoListUseCase: AddListUseCase,
 ) : ViewModel() {
-  private val _state = MutableStateFlow(TodoListCreatorState())
+    private val _state = MutableStateFlow(TodoListCreatorState())
 
-  fun changeListNumber(newNumber: Int) {
-    _state.update { currentState ->
-      currentState.copy(
-        listNumber = newNumber
-      )
-    }
-  }
-
-  fun changeDeadline(date: LocalDate) {
-    _state.update { currentState ->
-      currentState.copy(
-        deadline = date,
-      )
-    }
-  }
-
-  fun changeDescription(description: String) {
-    _state.update { currentState ->
-      currentState.copy(
-        description = description
-      )
-    }
-  }
-
-  fun submit() {
-    val listNumber: Int? = _state.value.listNumber
-    val deadline: LocalDate? = _state.value.deadline
-    val description: String? = _state.value.description
-    if (listNumber != null && deadline != null && description != null) {
-      runBlocking {
-        launch {
-          addTodoListUseCase(
-            listNumber = listNumber,
-            deadline = deadline,
-            description = description,
-          )
+    fun changeListNumber(newNumber: Int) {
+        _state.update { currentState ->
+            currentState.copy(
+                listNumber = newNumber
+            )
         }
-      }
     }
-  }
+
+    fun changeDeadline(date: LocalDate) {
+        _state.update { currentState ->
+            currentState.copy(
+                deadline = date,
+            )
+        }
+    }
+
+    fun changeDescription(description: String) {
+        _state.update { currentState ->
+            currentState.copy(
+                description = description
+            )
+        }
+    }
+
+    suspend fun submit() = withContext(Dispatchers.IO) {
+        val listNumber: Int? = _state.value.listNumber
+        val deadline: LocalDate? = _state.value.deadline
+        val description: String? = _state.value.description
+        if (listNumber != null && deadline != null && description != null) {
+            addTodoListUseCase(
+                listNumber = listNumber,
+                deadline = deadline,
+                description = description,
+            )
+        }
+    }
 }
